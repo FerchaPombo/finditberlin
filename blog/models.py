ea5bfa988978e3d6ca1d81a642a33b6151f7b6a8
FerchaPombo <129217509+FerchaPombo@users.noreply.github.com>
2023-11-14 08:55:49 +0000

    save changes in order to make migrations

--- blog/models.py
+++ blog/models.py
@@ -5,17 +5,41 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"),(1, "Published"))
 
 # Create your models here.
+# check the location key and how to add it later, part of the Post class 
 
 class Post(models.Model):
-    title = models.CharField(max_lenth = 100, unique=True)
-    slug = models.SlugField(max_lenth= 100, unique=True)
+    title = models.CharField(max_length = 100, unique=True)
+    slug = models.SlugField(max_length= 100, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
-    updated_on = models.DateTimeField(author_now=True)
-    content = models.TextField(max_lenth=100)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField(max_length=100)
     featured_image = CloudinaryField('image', default="placeholder")
     excerpt = models.TextField(blank=True)
-    created_on = models.DateTimeField(autor_now_add=True)
-    status = models.IntegerField(choices=Status, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
-    location = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post_location")
-    
+    #location = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post_location")
+
+    class Meta: 
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.like.count()
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"This is {self.name} comment: {self.body}"
+
