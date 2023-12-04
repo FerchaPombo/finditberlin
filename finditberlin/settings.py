cac71592c8477bd73a30639f83a0df2dfaa17aa1
FerchaPombo <>
2023-12-04 10:06:38 +0000

    remake workspace and add django storages

--- finditberlin/settings.py
+++ finditberlin/settings.py
@@ -80,7 +80,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'finditberlin.urls'
