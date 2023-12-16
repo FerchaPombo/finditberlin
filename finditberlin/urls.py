4437cd9a582bdb4dfc61fa43ed5fb48f886be830
Fercha Pombo <ferchapombo@gmail.com>
2023-12-16 11:03:21 +0000

    attempt to add a users panel with the rest of CRUD functionallity

--- finditberlin/urls.py
+++ finditberlin/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path("summernote/", include("django_summernote.urls")),
     path('', include("blog.urls"), name='blog_urls'),
     path('accounts/', include('allauth.urls')),
+    #path('members/', include('django.contrib.auth.urls')),
+    #path('', include('members.urls')),
     #path('googlemaps/', include('googlemaps.urls')),
 ]
