ba1b97fa2c58e9a7681a9fed7fdbe7766c0bbcad
Fercha Pombo <ferchapombo@gmail.com>
2023-12-19 11:32:46 +0000

    editing the post_detail.html for the changes in views. now they are function based views

--- blog/views.py
+++ blog/views.py
@@ -14,8 +14,112 @@ class PostList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 6
 
+def post_detail(request, slug, *args, **kwargs):
+    """
+    A function-based view to view the detail of a post.
+    Largely the same as the class-based, but we don't have
+    different methods for GET and POST. Because it's not a
+    class, all of the extra "self" stuff is removed too.
 
-class PostDetail(View):
+    Functionally, it's the same, but it is a bit clearer
+    what's going on. To differentiate between request methods,
+    we use request.method == "GET" or request.method == "POST"
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved = True).count()
+    liked = False
+    commented = False
+
+    if post.likes.filter(id=request.user.id).exists():
+        liked = True
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment awaiting moderation.')
+        else:
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(
+        request,
+        "post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "liked": liked,
+            "comment_form": comment_form
+        },
+    )
+
+
+def post_like(request, slug, *args, **kwargs):
+    """
+    The view to update the likes. Although it should always be
+    called using the POST method, we have still added some
+    defensive programming to make sure.
+    """
+    post = get_object_or_404(Post, slug=slug)
+
+    if request.method == "POST" and request.user.is_authenticated:
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id, *args, **kwargs):
+    """
+    view to delete comment
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset)
+    comment = post.comments.filter(id=comment_id).first()
+
+    if comment.name == request.user.username:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+def comment_edit(request, slug, comment_id, *args, **kwargs):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = post.comments.filter(id=comment_id).first()
+
+        comment_form = CommentForm(data=request.POST, instance=comment)
+        if comment_form.is_valid() and comment.name == request.user.username:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+'''class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
@@ -105,3 +209,4 @@ class PostLocation(View):
 
         # Return a JSON response indicating success
         return JsonResponse({"message": "Location updated successfully"})
+'''
