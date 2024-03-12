13b7d909366105ddbf8195915aa7aff68e9e700e
Fercha Pombo <ferchapombo@gmail.com>
2024-03-12 16:12:11 +0000

    properly linked the style.css

--- finditberlin/settings.py
+++ finditberlin/settings.py
@@ -88,7 +88,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'finditberlin.urls'
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+ 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
